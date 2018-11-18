d60bb90006fe7570ee2f3dfa772314e6c22dedc2
Serega <serv3546@gmail.com>
2018-11-17 19:07:29 -0800

    Added Parameters XML example

---
+++ src/main/java/core/Testng/testclasses/ParametersXML.java
@@ -0,0 +1,25 @@
+package core.Testng.testclasses;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ParametersXML {
+
+	@BeforeClass
+	@org.testng.annotations.Parameters({ "browser", "platform" })
+	public void beforeClass(String browser, String platform) {
+		System.out.println("Browser = " + browser);
+		System.out.println("Platform = " + platform);
+	}
+
+	@Test
+	@org.testng.annotations.Parameters({ "response" })
+	public void test(String response) {
+		String[] arr = response.split(",");
+		for (String responseCode : arr) {
+			System.out.println("Response code = " + responseCode);
+		}
+
+	}
+
+}
