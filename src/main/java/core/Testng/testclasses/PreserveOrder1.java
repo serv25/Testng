38525be1004dc0f03a8a2a08c1cd6ca7bc19176b
Serega <serv3546@gmail.com>
2018-11-17 16:33:09 -0800

    Preserve Order of Execution

---
+++ src/main/java/core/Testng/testclasses/PreserveOrder1.java
@@ -0,0 +1,16 @@
+package core.Testng.testclasses;
+
+import org.testng.annotations.Test;
+
+public class PreserveOrder1 {
+
+	@Test
+	public void test1() {
+		System.out.println("Test 1: " + this.getClass().getSimpleName());
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test 2: " + this.getClass().getSimpleName());
+	}
+}
